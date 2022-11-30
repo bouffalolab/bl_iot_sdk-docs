ca2df964be01bf24ba08138c5605ea536968765a
wyhong <wyhong@bouffalolab.com>
2022-11-30 10:29:02 +0800

    sync from bl_iot_sdk commit 7ebce23585ce59bf1fdd5fd53aee84336e3fb77f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'release_bl_iot_sdk_1.6.36',
+    VERSION: 'release_bl_iot_sdk_1.6.39-238-gf5ba0a7ee',
     LANGUAGE: 'zh',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
