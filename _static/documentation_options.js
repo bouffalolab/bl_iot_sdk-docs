4af4782740704dcfbe60818fd0c14ab031fb2424
wyhong <wyhong@bouffalolab.com>
2023-01-19 11:17:43 +0800

    bouffalolab_release_bl_iot_sdk_1.6.40-726-g0f9d027d5

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'release_bl_iot_sdk_1.6.39-238-gf5ba0a7ee',
+    VERSION: 'release_bl_iot_sdk_1.6.40-726-g0f9d027d5',
     LANGUAGE: 'zh',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
